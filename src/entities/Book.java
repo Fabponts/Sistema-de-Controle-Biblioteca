b3c4fda6cff780e446dbef6d78a8ee9509f0f770
VIVIANE ALVES DE PONTES <vivianealvesdepontes@MacBook-Air-de-VIVIANE.local>
2025-05-23 11:13:28 -0300

    created toString and update the Book class

--- src/entities/Book.java
+++ src/entities/Book.java
@@ -1,29 +1,31 @@
 package entities;
-import service.Library;
+
 import java.util.Scanner;
 
 public class Book {
 
-    // Atributos
     private String title;
     private String author;
+    private String genre;
+    private String editor;
     private BookStatus status;
 
     Scanner scan = new Scanner(System.in);
 
-    //construtores
     public Book(){
     }
-    public Book(String title,String author, BookStatus status) {
+    public Book(String title,String author,String genre,String editor, BookStatus status) {
         this.title = title;
         this.author = author;
+        this.genre = genre;
+        this.editor = editor;
         this.status = status;
     }
 
-    //getters and setters;
     public String getTitle() {
         return title;
     }
+
     public void setTitle(String title){
         this.title = title;
     }
@@ -31,14 +33,44 @@ public class Book {
     public String getAuthor() {
         return author;
     }
+
     public void setAuthor(String author){
         this.author = author;
     }
+
     public BookStatus getStatus(){
         return status;
     }
+
     public void setStatus(){
         this.status = status;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public String getEditor() {
+        return editor;
+    }
+
+    public void setEditor(String editor) {
+        this.editor = editor;
+    }
+
+    @Override
+    public String toString(){
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Book title: ").append(getTitle()).append("/n");
+        stringBuilder.append("Book author: ").append(getAuthor()).append("/n");
+        stringBuilder.append("Book genre: ").append(getGenre()).append("/n");
+        stringBuilder.append("Book editor: ").append(getEditor()).append("/n");
+        stringBuilder.append("Book status: ").append(getStatus()).append("/n");
+        return stringBuilder.toString();
     }
 }
 
